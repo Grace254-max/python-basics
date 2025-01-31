58a9d5b6fedd060c93dcca5a47f17312a681de0a
Grace254-max <mukoshigrace@gmail.com>
2025-01-31 12:46:09 +0300

    ggg

---
+++ basic.py
@@ -0,0 +1,26 @@
+#display text
+print("this is a python class")
+print("hello world")
+print(67)
+#variables
+#variables are name containers for storing values
+fname="grace"
+lname="mark"
+age=17
+course="cybersecurity"
+_favfood="chapati"
+
+#print the values
+print(fname)
+print(lname)
+print(age)
+print(course)
+print(_favfood)
+#displaying a string and variable
+print("hello, my name is",fname)
+print("i am" , age, "years old")
+print("i am learning", course)
+print("my name is" ,fname,lname,"and i am" ,age, "years old")
+#reasigning a variable value
+course="fullstack"
+print ("i am now learning" ,course)
